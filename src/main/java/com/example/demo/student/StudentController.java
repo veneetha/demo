b9a25eca8faaf283255bcb3d849946be6b15be63
veneetha <veneetha.c@dwpglobalcorp.com>
2023-02-01 18:20:25 +0530

    added api

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -24,11 +24,19 @@ public class StudentController {
     }
     @GetMapping(value = "/getfile")
     public List<HashMap<String,Object>> getfiles() {
+    	List<HashMap<String,Object>> mainlist= new ArrayList();
+    	HashMap<String,Object> maplist = new HashMap<>();
     	HashMap<String,Object> map = new HashMap<>();
     	map.put("hellooo","world");
+    	map.put("lineNo", "33");
+    	map.put("message", "HardCodedSensitiveValue");
+    	map.put("code","public static final String PARAM_HOST_DESCRIPTION = \"The domain name or the IP address of the data server\";");
     	List<HashMap<String,Object>> list= new ArrayList();
     	list.add(map);
-         return list;
+    	maplist.put("findings",list);
+    	maplist.put("fileName", "DataSourceConnector.java");
+    	mainlist.add(maplist);
+         return mainlist;
     }
     @GetMapping(value = "")
     public List<Student> getStudents() {
