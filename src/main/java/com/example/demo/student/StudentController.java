6ebe14f0c13b36ab0d1f91a3c452bb6e10992a4d
veneetha <veneetha.c@dwpglobalcorp.com>
2023-02-01 17:58:20 +0530

    added deploy changes

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,15 +1,18 @@
 package com.example.demo.student;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@RequestMapping(path = "/api/v1/student")
+//@RequestMapping(path = "/api/v1/student")
 public class StudentController {
 
     private final StudentService studentService;
